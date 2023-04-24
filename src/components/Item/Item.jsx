dfe7fb10b42162fa9a4ed67af1e7dc7f85b794b3
fortanelfr <fortanelfr@gmail.com>
2023-04-24 10:01:25 -0600

    Cambio en el orden de la información

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -5,9 +5,9 @@ import { Link } from 'react-router-dom'
 const Item = ({id, nombre, precio, img}) => {
   return (
     <div className='cardProducto'>
-        <p>Id: {id} </p>
         <p>Nombre: {nombre} </p>
         <p>Precio: {precio} </p>
+        <p>Id: {id} </p>
         <img className='imgProducto' src={img} alt={nombre} />
         {
           //<button className='btnProducto' >Mas detalles</button>
--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -3,7 +3,7 @@ import './ItemDetail.css'
 const ItemDetail = ({id, nombre, precio, img}) => {
   return (
     <div className='contenedorItem'>
-        <h2>Nombre: {nombre} </h2>
+        <h2>{nombre} </h2>
         <h3>Precio: {precio} </h3>
         <h3> ID: {id} </h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam iste reiciendis tempora unde! Quam commodi esse dicta odit nobis, enim, accusamus corrupti veritatis, excepturi aliquam laudantium? Veritatis ex sapiente deserunt.</p>
